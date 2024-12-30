043505751bf42fe8b2cc717f014303a6c08eccd4
safiya nasir <lci2023053@iiitl.ac.in>
2024-12-30 18:16:58 +0530

    Updated Sponsor

--- components/Sponsors/page.tsx
+++ components/Sponsors/page.tsx
@@ -1,11 +1,14 @@
-import { JSX } from "react";
-import styles from "./page.module.css";
+import React from "react";
+import styles from "./page.module.css"; 
 
- function Sponsors(): JSX.Element {
+const Carousel: React.FC = () => {
   return (
-    <div className={styles.page} id="Sponsors">
-      <h1 className={styles.heading}>SPONSORS</h1>
-      <div className={styles.Elements}>
+
+    <div className={styles.container}>
+     <div></div>
+      <div className={styles.carousel}>
+     
+        <div className={styles.carouselFace}>
         <a href="#" target="_blank" rel="noopener noreferrer">
           <div className={styles.card}>
             <div className={styles.wrapper}>
@@ -22,7 +25,8 @@ import styles from "./page.module.css";
             />
           </div>
         </a>
-
+        </div>
+        <div className={styles.carouselFace}>
         <a href="#" target="_blank" rel="noopener noreferrer">
           <div className={styles.card}>
             <div className={styles.wrapper}>
@@ -33,13 +37,106 @@ import styles from "./page.module.css";
               />
             </div>
             <img
-              src="/barbiQ-removebg-preview.png"
+              src="barbiQ-removebg-preview.png"
               alt="Force Mage Character"
               className={styles.character}
             />
           </div>
         </a>
-
+        </div>
+        <div className={styles.carouselFace}>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <div className={styles.card}>
+            <div className={styles.wrapper}>
+              <img
+                src="https://www.dhani.com/services/offers/assets/img/Lensfit.png"
+                alt="Force Mage Cover"
+                className={styles.coverImage}
+              />
+            </div>
+            <img
+              src="lenss.png"
+              alt="Force Mage Character"
+              className={styles.character}
+            />
+          </div>
+        </a>
+        </div>
+        <div className={styles.carouselFace}>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <div className={styles.card}>
+            <div className={styles.wrapper}>
+              <img
+                src="https://yt3.googleusercontent.com/-SqWW5wu-_p90A2gCAAD8NtrtHVz43jrq84P4BYHRkNrGbRBNqYwnUY2s4SKPbsuio-Elt8ONg=s900-c-k-c0x00ffffff-no-rj"
+                alt="Force Mage Cover"
+                className={styles.coverImage}
+              />
+            </div>
+            <img
+              src="/SBI-logo.svg-removebg-preview.png"
+              alt="Force Mage Character"
+              className={styles.character}
+            />
+          </div>
+        </a>
+        </div>
+        <div className={styles.carouselFace}>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <div className={styles.card}>
+            <div className={styles.wrapper}>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoYWYSsswAMfc1xGX8JPHHit9zNFZFPl4BaQ&s"
+                alt="Force Mage Cover"
+                className={styles.coverImage}
+              />
+            </div>
+            <img
+              src="barbiQ-removebg-preview.png"
+              alt="Force Mage Character"
+              className={styles.character}
+            />
+          </div>
+        </a>
+        </div>
+        <div className={styles.carouselFace}>
+        
+          <a href="#" target="_blank" rel="noopener noreferrer">
+          <div className={styles.card}>
+            <div className={styles.wrapper}>
+              <img
+                src="https://www.dhani.com/services/offers/assets/img/Lensfit.png"
+                alt="Force Mage Cover"
+                className={styles.coverImage}
+              />
+            </div>
+            <img
+              src="lenss.png"
+              alt="Force Mage Character"
+              className={styles.character}
+            />
+          </div>
+        </a>
+        
+        </div>
+        <div className={styles.carouselFace}>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <div className={styles.card}>
+            <div className={styles.wrapper}>
+              <img
+                src="https://yt3.googleusercontent.com/-SqWW5wu-_p90A2gCAAD8NtrtHVz43jrq84P4BYHRkNrGbRBNqYwnUY2s4SKPbsuio-Elt8ONg=s900-c-k-c0x00ffffff-no-rj"
+                alt="Force Mage Cover"
+                className={styles.coverImage}
+              />
+            </div>
+            <img
+              src="/SBI-logo.svg-removebg-preview.png"
+              alt="Force Mage Character"
+              className={styles.character}
+            />
+          </div>
+        </a>
+        </div>
+        <div className={styles.carouselFace}>
         <a href="#" target="_blank" rel="noopener noreferrer">
           <div className={styles.card}>
             <div className={styles.wrapper}>
@@ -56,25 +153,29 @@ import styles from "./page.module.css";
             />
           </div>
         </a>
-
+        </div>
+        <div className={styles.carouselFace}>
         <a href="#" target="_blank" rel="noopener noreferrer">
           <div className={styles.card}>
             <div className={styles.wrapper}>
               <img
-                src="https://www.dhani.com/services/offers/assets/img/Lensfit.png"
+                src="https://yt3.googleusercontent.com/-SqWW5wu-_p90A2gCAAD8NtrtHVz43jrq84P4BYHRkNrGbRBNqYwnUY2s4SKPbsuio-Elt8ONg=s900-c-k-c0x00ffffff-no-rj"
                 alt="Force Mage Cover"
                 className={styles.coverImage}
               />
             </div>
             <img
-              src="/lenss.png"
+              src="/SBI-logo.svg-removebg-preview.png"
               alt="Force Mage Character"
               className={styles.character}
             />
           </div>
         </a>
+        </div>
       </div>
+      <div id="ground"></div>
     </div>
   );
-}
-export default Sponsors;
+};
+
+export default Carousel;
